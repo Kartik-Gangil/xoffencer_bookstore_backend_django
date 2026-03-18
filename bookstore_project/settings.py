64472d03f2d761a70a45c55280e6b4581de41901
kartikgupta666 <kartikgangil@gmail.com>
2026-03-18 19:37:50 +0530

    add host

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.235.229.202']
 
 
 # Application definition
